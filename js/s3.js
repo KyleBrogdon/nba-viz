80d32068cd2c867466a872ea1f0d3f151d1afdf5
kylebrogdon <kbrogdon44@gmail.com>
2024-07-20 13:35:10 -0500

    fixing s3

--- js/s3.js
+++ js/s3.js
@@ -16,50 +16,59 @@ export function loadS3() {
             console.log(`Team: ${d.Team}, Wins: ${d.Wins}, Followers: ${d.Followers}`);
         });
 
-    d3.csv("data/teams.csv").then(data => {
-        const margin = { top: 20, right: 20, bottom: 50, left: 60 };
-        const width = 960 - margin.left - margin.right;
+        const margin = { top: 20, right: 20, bottom: 150, left: 150 };
+        const width = 1400 - margin.left - margin.right;
         const height = 500 - margin.top - margin.bottom;
-        
 
         const svg = container.append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
-            .append("g")
+          .append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
 
         const x = d3.scaleLinear()
             .range([0, width])
-            .domain([0, d3.max(data, d => +d.wins)]);
+            .domain([0, d3.max(data, d => d.Wins)]);
 
         const y = d3.scaleLinear()
             .range([height, 0])
-            .domain([0, d3.max(data, d => +d.followers)]);
-
-        svg.append("g")
-            .selectAll("circle")
-            .data(data)
-          .enter().append("circle")
-            .attr("cx", d => x(d.wins))
-            .attr("cy", d => y(d.followers))
-            .attr("r", 5)
-            .style("fill", d => teamColors[d.Team]);
+            .domain([0, d3.max(data, d => d.Followers)]);
 
         svg.append("g")
             .attr("transform", `translate(0,${height})`)
-            .call(d3.axisBottom(x).ticks(10).tickFormat(d3.format("d")));
+            .call(d3.axisBottom(x));
 
         svg.append("g")
             .call(d3.axisLeft(y));
 
+        svg.selectAll("circle")
+            .data(data)
+          .enter().append("circle")
+            .attr("cx", d => {
+                const cx = x(d.Wins);
+                console.log(`Team: ${d.Team}, Wins: ${d.Wins}, CX: ${cx}`); 
+                return cx;
+            })
+            .attr("cy", d => {
+                const cy = y(d.Followers);
+                console.log(`Team: ${d.Team}, Followers: ${d.Followers}, CY: ${cy}`); 
+                return cy;
+            })
+            .attr("r", 5)
+            .style("fill", d => {
+                const color = teamColors[d.Team] || "#69b3a2";
+                console.log(`Team: ${d.Team}, Color: ${color}`); 
+                return color;
+            });
+
         svg.append("text")
-            .attr("transform", `translate(${width / 2},${height + margin.bottom - 10})`)
+            .attr("transform", `translate(${width / 2},${height + margin.bottom - 40})`)
             .style("text-anchor", "middle")
             .text("Team Wins");
 
         svg.append("text")
             .attr("transform", "rotate(-90)")
-            .attr("y", 0 - margin.left)
+            .attr("y", 0 - margin.left + 60)
             .attr("x", 0 - height / 2)
             .attr("dy", "1em")
             .style("text-anchor", "middle")
