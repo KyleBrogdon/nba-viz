90f7b6676d02c2d9686b53c7d6bdc2ac9a9f18e7
kylebrogdon <kbrogdon44@gmail.com>
2024-07-19 22:07:12 -0500

    style changes s2

--- js/s2.js
+++ js/s2.js
@@ -7,12 +7,6 @@ function loadS2() {
     container.append("h1").text("NBA Team Instagram Followers");
 
     d3.csv("data/teams.csv").then(data => {
-        if (data.length > 0) {
-            console.log("CSV Data Loaded. Column Headers: ", Object.keys(data[0]));
-        } else {
-            console.error("CSV Data is empty or not loaded correctly.");
-        }
-
         data.forEach(d => {
             d.Followers = +d.Followers;
             if (isNaN(d.Followers)) {
@@ -22,9 +16,10 @@ function loadS2() {
             }
         });
 
+
         data.sort((a, b) => d3.descending(a.Followers, b.Followers));
 
-        const margin = { top: 20, right: 20, bottom: 150, left: 60 };
+        const margin = { top: 20, right: 20, bottom: 150, left: 100 };
         const width = 960 - margin.left - margin.right;
         const height = 500 - margin.top - margin.bottom;
 
@@ -36,15 +31,12 @@ function loadS2() {
 
         const x = d3.scaleBand()
             .range([0, width])
-            .padding(0.1)
+            .padding(0.2) 
             .domain(data.map(d => d.Team));
 
         const y = d3.scaleLinear()
             .range([height, 0])
             .domain([0, d3.max(data, d => d.Followers)]);
-
-        const color = d3.scaleOrdinal(d3.schemeCategory10)
-            .domain(data.map(d => d.Team));
 
         svg.append("g")
             .selectAll(".bar")
@@ -54,15 +46,8 @@ function loadS2() {
             .attr("x", d => x(d.Team))
             .attr("width", x.bandwidth())
             .attr("y", d => y(d.Followers))
-            .attr("height", d => {
-                const barHeight = height - y(d.Followers);
-                console.log(`Team: ${d.Team}, Height: ${barHeight}`);
-                if (isNaN(barHeight)) {
-                    console.error(`NaN height for ${d.Team}`);
-                }
-                return barHeight;
-            })
-            .attr("fill", d => color(d.Team));
+            .attr("height", d => height - y(d.Followers))
+            .attr("fill", d => teamColors[d.Team] || "#69b3a2");
 
         svg.append("g")
             .attr("transform", `translate(0,${height})`)
@@ -75,13 +60,13 @@ function loadS2() {
             .call(d3.axisLeft(y));
 
         svg.append("text")
-            .attr("transform", `translate(${width / 2},${height + margin.bottom - 10})`)
+            .attr("transform", `translate(${width / 2},${height + margin.bottom - 40})`)
             .style("text-anchor", "middle")
             .text("Team");
 
         svg.append("text")
             .attr("transform", "rotate(-90)")
-            .attr("y", 0 - margin.left)
+            .attr("y", 0 - margin.left + 20)
             .attr("x", 0 - height / 2)
             .attr("dy", "1em")
             .style("text-anchor", "middle")
