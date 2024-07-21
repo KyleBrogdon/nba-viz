f1dbfb973cb037a8856ad1c022d76a3c74f7390a
kylebrogdon <kbrogdon44@gmail.com>
2024-07-20 20:39:49 -0500

    fix s1

--- js/s1.js
+++ js/s1.js
@@ -6,7 +6,13 @@ export function loadS1() {
 
     container.append("h1").text("NBA Team and Player Success Compared To Social Media popularity");
 
-    container.append("p").text("Box Plus-Minus (BPM) is a box score-based metric for evaluating basketball players' quality and contribution to the team. Instagram followers indicate the popularity of players on social media.");
+    container.append("img")
+        .attr("src", "data/cover.jpg")
+        .attr("alt", "Cover Image")
+        .attr("width", "100%") 
+        .attr("height", "auto"); 
+
+    container.append("p").text("This interactive slideshow will compare the most popular NBA Teams and Players on social media (using Instagram) with both team and individual success. Primary metrics used are team wins, team instagram followers, player instagram followers, and Box Plus-Minus (BPM). BPM is a box score-based metric for evaluating basketball players' quality and contribution to the team.");
 
     container.append("button")
         .text("Next")
