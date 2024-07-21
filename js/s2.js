05eb78b47b2d9a78ff3b501e906d17c1876f3bfd
kylebrogdon <kbrogdon44@gmail.com>
2024-07-20 21:06:14 -0500

    fix spacing

--- js/s2.js
+++ js/s2.js
@@ -16,9 +16,9 @@ export function loadS2() {
         teamsData.sort((a, b) => b.Followers - a.Followers);
 
         d3.csv("data/merged_players_bpm.csv").then(mergedPlayers => {
-            const margin = { top: 30, right: 20, bottom: 90, left: 100 };
+            const margin = { top: 30, right: 20, bottom: 130, left: 100 };
             const width = 1400 - margin.left - margin.right;
-            const height = 900 - margin.top - margin.bottom;
+            const height = 700 - margin.top - margin.bottom;
 
             const svg = container.append("svg")
                 .attr("width", width + margin.left + margin.right)
